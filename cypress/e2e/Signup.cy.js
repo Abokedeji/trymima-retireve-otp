4942d773d8b18504c478a130aaa086f47443f781
Obos <okedejiabiodun@gmail.com>
2025-05-13 21:41:47 +0100

    Update test Scenarios

--- cypress/e2e/Signup.cy.js
+++ cypress/e2e/Signup.cy.js
@@ -5,5 +5,17 @@ describe('Signup', () => {
         cy.CompleteSignupForm()
         cy.retrieveAndInsertOTP()
     });
+
+    it('Signup filling one optional field - Business Registration Number', () => {
+        
+    });
+
+    it('Signup filling one optional field - Instagram handle', () => {
+        
+    });
+
+    it('Signup filling one optional field - Twitter handle', () => {
+        
+    });
 });
             
